8510248f19222f57c4789bc07b81ad090fb0f009
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-06-22 20:03:44 +0300

    Added Loops in looping.py file

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i>0:
+        if i == 1:
+            print(i)
+            print("Happy New Year!")
+        else:
+            print(i)
+        i -= 1
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list = [element * element for element in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(100):
+        i+=1
+        if (i%3 == 0 and i%5 == 0):
+            print("FizzBuzz")
+        elif i%3 == 0:
+            print("Fizz")
+        elif i%5 == 0:
+            print("Buzz")
+        else:
+            print(i)
